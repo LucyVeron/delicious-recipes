f490427828f99517bfe3455e799e01aee67a908c
Lucy Veron <lucymveron@gmail.com>
2023-01-15 12:08:43 +0100

    Slide styling

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import styled from 'styled-components';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
 
 function Popular() {
 
@@ -12,19 +15,68 @@ function Popular() {
         const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9 `);
         const data = await api.json();
         setPopular(data.recipes);
+        console.log(data.recipes);
     };
 
     return (
         <div>
-            {popular.map((recipe) => {
-                return (
-                    <div>
-                        <p>{recipe.title}</p>
-                    </div>
-                );
-            })}
+            <Wrapper>
+                <h3>Popular Picks</h3>
+                <Splide options={{
+                    perPage: 4,
+                    arrows: false,
+                    pagination: false,
+                    drag: 'free',
+                    gap: '5rem'
+                }}>
+                    {popular.map((recipe) => {
+                        return (
+                            <SplideSlide key={recipe.id}>
+                                <Card>
+                                    <p>{recipe.title}</p>
+                                    <img src={recipe.image} alt={recipe.title} />
+                                </Card>
+                            </SplideSlide>
+                        );
+                    })}
+                </Splide>
+            </Wrapper>
         </div>
     )
 }
+
+const Wrapper = styled.div`
+    margin: 4rem 0
+`;
+
+const Card = styled.div`
+    min-height: 25rem;
+    overflow: hidden;
+    position: relative;
+    img {
+        border-radius: 2rem;
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+    p {
+        position: absolute;
+        z-index: 10;
+        left: 50%;
+        bottom: 0%;
+        transform: translate(-50%, 0%);
+        color: white;
+        width: 100%;
+        text-align: center;
+        font-weight: 600;
+        font-size: 1rem;
+        height: 40%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`;
 
 export default Popular
