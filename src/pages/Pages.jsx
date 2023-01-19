ea92ecbfd87165392d7a91f7e8b3db5e6d07bb1d
Lucy Veron <lucymveron@gmail.com>
2023-01-19 13:49:47 +0100

    Code splitting

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import Home from './Home'
+/* import Home from './Home' */
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Cuisine from './Cuisine'
 import Searched from './Searched'
 import Recipe from './Recipe'
 import { AnimatePresence } from 'framer-motion';
+
+const Home = React.lazy(() => import("./Home"));
+/* const Searched = React.lazy(() => import("./Searched")); */
 
 function Pages() {
     const location = useLocation();
@@ -12,6 +15,7 @@ function Pages() {
         <AnimatePresence mode="wait">
             <Routes Location={location} key={location.path}>
                 <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
                 <Route path='/cuisine/:type' element={<Cuisine />} />
                 <Route path='/searched/:search' element={<Searched />} />
                 <Route path='/recipe/:id' element={<Recipe />} />
